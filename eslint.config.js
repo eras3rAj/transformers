bf9dd98b378a22754a807feeffa9a0479123c3c8
Anmol <anmol@voltforge.com>
2026-06-08 17:35:20 +0530

    fix: Change unused imports rule to warn to fix Vercel deployments

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,7 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-imports': 'warn',
       'unused-imports/no-unused-vars': [
         'warn',
         { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
